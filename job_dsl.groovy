faca3da95b0b98922d8a4b088333092e422eba75
Alexis Faure <alexis.faure@epitech.eu>
2023-02-24 13:50:46 +0100

    Job factory ok

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -15,5 +15,46 @@ folder('Tools') {
             stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
             stringParam('DISPLAY_NAME', '', 'Display name for the job')
         }
+        steps {
+            dsl {
+                filename('job_dsl.groovy')
+                additionalParameters([
+                    'GITHUB_NAME': '${.GITHUB_NAME}',
+                    'JOB_DISPLAY_NAME': '${params.DISPLAY_NAME}'
+                ])
+            }
+        }
+    }
+}
+
+class JobDslScript implements DSL {
+    void apply(DslFactory factory) {
+        def GITHUB_NAME = factory.stringParam('GITHUB_NAME', '')
+        def JOB_DISPLAY_NAME = factory.stringParam('JOB_DISPLAY_NAME', '')
+        factory.job(JOB_DISPLAY_NAME) {
+            displayName(JOB_DISPLAY_NAME)
+            wrappers {
+                preBuildCleanup()
+            }
+            properties {
+                githubProjectUrl('https://github.com/${GITHUB_NAME}')
+            }
+            scm {
+                git {
+                    remote {
+                        url('${GITHUB_NAME}', 'main')
+                    }
+                }
+            }
+            triggers {
+                scm('* * * * *')
+            }
+            steps {
+                shell('make fclean')
+                shell('make')
+                shell('make tests_run')
+                shell('make clean')
+            }
+        }
     }
 }
